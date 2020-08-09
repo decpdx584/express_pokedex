bd723fafe4469322fd13228287ee5a8e4a8130ef
General Assembly <generalassembly@Davids-MacBook-Pro.local>
2020-08-09 15:29:43 -0700

    [faves] Post favorites to DB

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,33 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../models')
+
 
 // GET /pokemon - return a page with favorited Pokemon
-router.get('/', function(req, res) {
-  // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+router.get('/', async (req, res) => {
+  try {
+    const foundPokemons = await db.pokemon.findAll();
+    res.render('favorites', {
+      pokemon: foundPokemons
+    });
+    } catch (err) {
+      res.render('err');
+  }
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
-router.post('/', function(req, res) {
+router.post('/', async (req, res) => {
   // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  try {
+    await db.pokemon.findOrCreate({
+      where: {
+        name: req.body.name
+      }
+    })
+    res.redirect('/pokemon');
+  } catch (err) {
+    res.render('err');
+  }
 });
 
 module.exports = router;
